9037d0ff248555cef9915f9212a4fc34c17159ec
yash-copods <yash.thakur@copods.co>
2025-07-21 16:08:51 +0530

    Initial commit from create-liveblocks-app

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { getUser } from "@/lib/database";
+import { User } from "./types";
 
 export const authConfig: NextAuthConfig = {
   // Configure one or more authentication providers
@@ -21,7 +22,7 @@ export const authConfig: NextAuthConfig = {
           throw new Error("No credentials or email");
         }
 
-        const user = await getUser(credentials.email);
+        const user: User | null = await getUser(credentials.email);
 
         if (!user) {
           throw new Error("User not found");
@@ -35,27 +36,6 @@ export const authConfig: NextAuthConfig = {
         };
       },
     }),
-
-    /*
-    // Use GitHub authentication
-    // import GithubProvider from "next-auth/providers/github";
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    */
-
-    /*
-    // Use Auth0 authentication
-    // import Auth0Provider from "next-auth/providers/auth0";
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID as string,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-      issuer: process.env.AUTH0_ISSUER_BASE_URL,
-    }),
-    */
-
-    // ...add more providers here
   ],
 
   trustHost: true,
