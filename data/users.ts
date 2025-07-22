9e63c715c37cd7247fcfa9c8f32f561ba4c5aa35
yash-copods <yash.thakur@copods.co>
2025-07-22 14:45:49 +0530

    added user

--- data/users.ts
+++ data/users.ts
@@ -9,42 +9,41 @@ import { User } from "@/types";
  * Group info is in /data/groups.ts
  */
 export const users: Omit<User, "color">[] = [
-  /*
   {
-    id: "[YOUR EMAIL ADDRESS]",
-    name: "[YOUR DISPLAY NAME]",
+    id: "yash.thakur@copods.co",
+    name: "yash thakur",
     avatar: "https://liveblocks.io/avatars/avatar-0.png",
     groupIds: ["product", "engineering", "design"],
   },
-  */
-  {
-    id: "charlie.layne@example.com",
-    name: "Charlie Layne",
-    avatar: "https://liveblocks.io/avatars/avatar-2.png",
-    groupIds: ["product", "engineering", "design"],
-  },
-  {
-    id: "mislav.abha@example.com",
-    name: "Mislav Abha",
-    avatar: "https://liveblocks.io/avatars/avatar-3.png",
-    groupIds: ["engineering"],
-  },
-  {
-    id: "tatum.paolo@example.com",
-    name: "Tatum Paolo",
-    avatar: "https://liveblocks.io/avatars/avatar-4.png",
-    groupIds: ["engineering", "design"],
-  },
-  {
-    id: "anjali.wanda@example.com",
-    name: "Anjali Wanda",
-    avatar: "https://liveblocks.io/avatars/avatar-5.png",
-    groupIds: ["product"],
-  },
-  {
-    id: "emil.joyce@example.com",
-    name: "Emil Joyce",
-    avatar: "https://liveblocks.io/avatars/avatar-6.png",
-    groupIds: ["product", "design"],
-  },
+
+  // {
+  //   id: "charlie.layne@example.com",
+  //   name: "Charlie Layne",
+  //   avatar: "https://liveblocks.io/avatars/avatar-2.png",
+  //   groupIds: ["product", "engineering", "design"],
+  // },
+  // {
+  //   id: "mislav.abha@example.com",
+  //   name: "Mislav Abha",
+  //   avatar: "https://liveblocks.io/avatars/avatar-3.png",
+  //   groupIds: ["engineering"],
+  // },
+  // {
+  //   id: "tatum.paolo@example.com",
+  //   name: "Tatum Paolo",
+  //   avatar: "https://liveblocks.io/avatars/avatar-4.png",
+  //   groupIds: ["engineering", "design"],
+  // },
+  // {
+  //   id: "anjali.wanda@example.com",
+  //   name: "Anjali Wanda",
+  //   avatar: "https://liveblocks.io/avatars/avatar-5.png",
+  //   groupIds: ["product"],
+  // },
+  // {
+  //   id: "emil.joyce@example.com",
+  //   name: "Emil Joyce",
+  //   avatar: "https://liveblocks.io/avatars/avatar-6.png",
+  //   groupIds: ["product", "design"],
+  // },
 ];
